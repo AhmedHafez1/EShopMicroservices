c4499c6183aef0afe76cee99cb5363fd947cd21e
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2024-08-17 16:03:54 +0300

    Get Product By Id

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,29 @@
+﻿
+using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdRequest(Guid Id);
+    public record GetProductByIdResponse(Product Product);
+    public class GetProductByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/{id}", async (GetProductByIdRequest request, ISender sender) =>
+            {
+                var query = request.Adapt<GetProductByIdQuery>();
+
+                var result = await sender.Send(query);
+
+                var response = result.Adapt<GetProductByIdResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("Get Product By Id")
+            .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithDescription("Get Product By Id")
+            .WithSummary("Get Product By Id");
+        }
+    }
+}
