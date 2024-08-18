6775279505d5e31d9bd9028990e60f1444526538
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2024-08-18 15:00:27 +0300

    Update and delete

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+    public record DeleteProductResult(bool IsSuccess);
+    public class DeleteProductCommandHandler(IDocumentSession session, ILogger<DeleteProductCommandHandler> logger) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+    {
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation($"Handler DeleteProductCommandHandler has been called with id {command.Id}");
+
+            var product = await session.LoadAsync<Product>(command.Id);
+
+            if (product == null)
+            {
+                throw new NotFoundException("Product not existing!");
+            }
+
+            session.Delete<Product>(command.Id);
+            await session.SaveChangesAsync();
+
+            return new DeleteProductResult(true);
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Catalog.API.Products.UpdateProduct
+{
+    public record UpdateProductCommand(Guid Id, string Name, string Description, List<string> Category, string ImagePath, decimal Price)
+        : ICommand<UpdateProductResult>;
+    public record UpdateProductResult(bool IsSuccess);
+    public class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+    {
+        public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation($"Handler UpdateProductCommandHandler has been called for product with id {command.Id}");
+            var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+            if (product is null)
+            {
+                throw new NotFoundException("Product not found!");
+            }
+
+            product.Name = command.Name;
+            product.Description = command.Description;
+            product.Category = command.Category;
+            product.ImagePath = command.ImagePath;
+            product.Price = command.Price;
+
+            session.Update(product);
+            await session.SaveChangesAsync();
+
+            return new UpdateProductResult(true);
+        }
+    }
+}
