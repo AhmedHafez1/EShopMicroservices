b094178b4f6eae332ba22c19367bdc7500702ec1
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2024-08-21 07:21:35 +0300

    Basket Models

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,18 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; } = default!;
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; } = new();
+        public decimal TotalPrice => ShoppingCartItems.Sum(x => x.Price * x.Quantity);
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public ShoppingCart()
+        {
+        }
+    }
+}
