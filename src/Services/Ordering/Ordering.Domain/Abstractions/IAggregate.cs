7f35db684888d185aeb09273d5b995e81488746d
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2024-09-12 15:02:50 +0300

    DDD

--- src/Services/Ordering/Ordering.Domain/Abstractions/IAggregate.cs
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IAggregate.cs
@@ -3,6 +3,7 @@
     public interface IAggregate<T> : IAggregate, IEntity<T>
     {
     }
+
     public interface IAggregate : IEntity
     {
         IReadOnlyList<IDomainEvent> DomainEvents { get; }
---
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,20 @@
+﻿using Ordering.Domain.Abstractions;
+
+namespace Ordering.Domain.Models
+{
+    public class OrderItem : Entity<Guid>
+    {
+        public OrderItem(decimal price, int quantity, Guid productId, Guid orderId)
+        {
+            Price = price;
+            Quantity = quantity;
+            ProductId = productId;
+            OrderId = orderId;
+        }
+
+        public decimal Price { get; private set; }
+        public int Quantity { get; private set; }
+        public Guid ProductId { get; private set; }
+        public Guid OrderId { get; private set; }
+    }
+}
