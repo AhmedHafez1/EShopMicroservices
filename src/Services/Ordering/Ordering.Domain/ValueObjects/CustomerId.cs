4354d3d4a78db16c6ec647c8cbb280151dc091d6
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2024-09-15 06:37:45 +0300

    DomainException

--- src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
@@ -1,7 +1,23 @@
-﻿namespace Ordering.Domain.ValueObjects
+﻿
+
+namespace Ordering.Domain.ValueObjects
 {
     public record CustomerId
     {
         public Guid Value { get; }
+
+        public CustomerId(Guid id) => Value = id;
+
+        public static CustomerId Of(Guid value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+
+            if (value == Guid.Empty)
+            {
+                throw new DomainException("Customer id cannot be empty!");
+            }
+
+            return new CustomerId(value);
+        }
     }
 }
