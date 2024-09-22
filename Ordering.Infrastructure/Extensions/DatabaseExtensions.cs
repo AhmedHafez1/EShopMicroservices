fed5c9172ef0b67722abde711ff7f4029beef930
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2024-09-22 07:05:10 +0300

    SeedAsync

--- Ordering.Infrastructure/Extensions/DatabaseExtensions.cs
+++ Ordering.Infrastructure/Extensions/DatabaseExtensions.cs
@@ -10,6 +10,40 @@ namespace Ordering.Infrastructure.Extensions
             var scope = app.Services.CreateScope();
             var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
             await context.Database.MigrateAsync();
+            await SeedAsync(context);
+        }
+
+        public static async Task SeedAsync(AppDbContext context)
+        {
+            await SeedCustomersAsync(context);
+            await SeedProductsAsync(context);
+            await SeedOrdersAndItemsAsync(context);
+        }
+
+        public static async Task SeedCustomersAsync(AppDbContext context)
+        {
+            if (!context.Customers.Any())
+            {
+                await context.Customers.AddRangeAsync(InitialData.Customers);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public static async Task SeedProductsAsync(AppDbContext context)
+        {
+            if (!context.Products.Any())
+            {
+                await context.Products.AddRangeAsync(InitialData.Products);
+                await context.SaveChangesAsync();
+            }
+        }
+        public static async Task SeedOrdersAndItemsAsync(AppDbContext context)
+        {
+            if (!context.Orders.Any())
+            {
+                await context.Orders.AddRangeAsync(InitialData.OrdersWithItems);
+                await context.SaveChangesAsync();
+            }
         }
     }
 }
