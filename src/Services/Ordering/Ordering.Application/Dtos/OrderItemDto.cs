1617b9c4e38e35eda261b33911d34c17acc2a792
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2024-09-26 15:19:18 +0300

    Dtos

---
+++ src/Services/Ordering/Ordering.Application/Dtos/OrderItemDto.cs
@@ -0,0 +1,8 @@
+﻿using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Application.Dtos
+{
+    public record OrderItemDto(ProductId ProductId, OrderId OrderId, decimal Price, int Quantity)
+    {
+    }
+}
